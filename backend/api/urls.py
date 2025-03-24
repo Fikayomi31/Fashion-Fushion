808c6a5946808df4dc3c92e146bea5f63c467fc0
Fikayomi31 <fikayoogundijo@gmail.com>
2025-03-24 14:38:34 -0700

    auth file for user register, login, logout and it also deal with token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-
 from userauth.views import (
     MyTokenObtainPairView,
     RegisterView,
@@ -14,9 +13,9 @@ from store import views as store_view
 
 urlpatterns = [
     # Authentication endpoints
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('user/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', RegisterView.as_view(), name='register'),
     
     # Password reset flow
     path('password-reset-email/<str:email>/', PasswordEmailVerify.as_view(), name='password_reset_email'),
