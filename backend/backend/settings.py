87c2cceef004ba0bd5c5d3b5fab383dd5b1b239a
Fikayomi31 <fikayoogundijo@gmail.com>
2025-09-29 18:42:02 -0700

    amending bugs in the user models only

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,12 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
-from environs import Env
-env = Env()
-env.read_env()
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
