9c6c9fbb90434e3ce1de9aeef2e1f10f6d589097
Fikayomi31 <fikayoogundijo@gmail.com>
2025-10-23 04:09:21 -0700

    Including sidebar in the customer account

--- frontend/src/views/customer/Account.jsx
+++ frontend/src/views/customer/Account.jsx
@@ -21,50 +21,7 @@ function Account() {
             <section className="">
             <div className="row">
                 
-                <div className="col-lg-3">
-                <div className="d-flex justify-content-center align-items-center flex-column mb-4 shadow rounded-3">
-                    <img
-                    src="https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2220431045.jpg"
-                    style={{ width: 120 }}
-                    alt=""
-                    />
-                    <div className="text-center">
-                    <h3 className="mb-0">{profile.full_name}</h3>
-                    <p className="mt-0">
-                        <a href="">Edit Account</a>
-                    </p>
-                    </div>
-                </div>
-                <ol className="list-group">
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">Account</div>
-                    </div>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">Orders</div>
-                    </div>
-                    <span className="badge bg-primary rounded-pill">14</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">Wishlist</div>
-                    </div>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">Notification</div>
-                    </div>
-                    <span className="badge bg-primary rounded-pill">14</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">Setting</div>
-                    </div>
-                    </li>
-                </ol>
-                </div>
+                <SideBar />
                 <div className="col-lg-9 mt-1">
                 <main className="mb-5" style={{}}>
                     <div className="container px-4">
