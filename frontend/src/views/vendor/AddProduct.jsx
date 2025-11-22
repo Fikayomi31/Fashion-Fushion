791e82f0778c2386b30bef9b27f1cc663e1875c9
Fikayomi31 <fikayoogundijo@gmail.com>
2025-11-21 16:50:47 -0800

    Enhance AddProduct component to support dynamic specifications, colors, sizes, and gallery images; implement input handling and image preview functionality

--- frontend/src/views/vendor/AddProduct.jsx
+++ frontend/src/views/vendor/AddProduct.jsx
@@ -20,7 +20,65 @@ function AddProduct() {
         stock_qty: '',
         vendor: userData?.vendor_id
     })
-    const [specification]
+    const [specification, setSpecification] = useState({title: '', content:''})
+    const [colors, setColors] = useState([{name: '', color_code: ''}])
+    const [sizes, setSizes] = useState()
+    const [gallery, setGallery] = useState([{image: ''}])
+    const [category, setCategory] = useState([])
+
+    const handleAddMore = (setStateFunction) => {
+        setStateFunction((prevState) => [...prevState, {}])
+    }
+
+    const handleRemove = (index, setStateFunction) => {
+        setStateFunction((prevState) => {
+            const newState = [...prevState]
+            newState.splice(index, 1)
+            return newState
+        })
+
+    }
+
+    const handleInputChange = (input, field, value, setStateFunction) => {
+        setStateFunction((prevState) => {
+            const newState = [...prevState]
+            newState[index][field] = value
+
+            return newState
+        })
+    }
+
+    const handleImageChange = (index, event, setStateFunction) => {
+        const file = event.target.files[0]
+        if (file) {
+            const reader = new FileReader()
+
+            reader.onloadend = () => {
+                setStateFunction((prevState) => {
+                    const newState = [...prevState]
+                    newState[index].image = {file, preview: reader.result}
+
+                    return newState
+
+                })  
+
+                
+            }
+            reader.readAsDataURL(file)
+        } else {
+            setStateFunction((prevState) => {
+                const newState = [...prevState]
+                newState[index].image = null
+                
+                newState[index].preview = null
+
+                return newState
+            })
+        }
+
+
+    }
+
 
   return (
     <div className="container-fluid" id="main">
